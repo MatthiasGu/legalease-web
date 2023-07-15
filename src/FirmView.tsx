ad76863613b1052e5b46e2283ecfc6036d6e2148
Motiejus Gudenas <mj.gudenas@gmail.com>
2023-07-15 16:04:32 +0100

    Star ratings

--- src/FirmView.tsx
+++ src/FirmView.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import './FirmView.css'
 
 export type IBadge = {
   id: string;
@@ -50,17 +51,57 @@ const FirmName = styled.h3`
   margin: 0;
 `
 
+const FirmRatingLabel = styled.label`
+  font-size: 12px;
+  text-transform: uppercase;
+`
+
+function convertRatingToStars(rating: number) {
+  const nearestHundred = Math.round(rating/100) * 100;
+  console.log(nearestHundred);
+  return nearestHundred / 200;
+}
+
+const StarRating: React.FC<{stars: number}> = ({ stars }) => {
+  console.log(stars);
+  return <div className="star-rating">
+    {[...Array(5)].map((star, idx) => {
+      let className = "star";
+      if (idx <= stars) {
+        className += "-filled";
+      } else if (idx === (stars + 0.5)) {
+        className += "-half";
+      }
+      return (
+        <span className={className}>&#9733;</span>
+      );
+    })}
+  </div>;
+}
+const FirmRating: React.FC<{firmRegion: IFirmRegion}> = ({firmRegion}) => {
+  return (
+    <Row>
+      <FirmRatingLabel> Expertise and reputation</FirmRatingLabel>
+      <StarRating stars={convertRatingToStars(firmRegion.expertiseAndReputationRating)} /> 
+      <FirmRatingLabel> Client Satisfaction</FirmRatingLabel>
+      <StarRating stars={convertRatingToStars(firmRegion.clientSatisfactionRating)} /> 
+    </Row>
+  )
+}
+
 const FirmView: React.FC<{firm: IFirm, tier: string}> = ({tier, firm}) => {
   console.log(firm);
 
   return (
+    <>
     <Row>
       <TierRanking> {tier} </TierRanking>
       <FirmName> {firm.name} </FirmName>
-      <Row>
-        
-      </Row>
     </Row>
+    <Row>
+      <FirmRating firmRegion = {firm.firmRegions[0]}/>
+    </Row>
+    </>
   )
 }
 
